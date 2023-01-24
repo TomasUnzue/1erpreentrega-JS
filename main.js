2c5809507028145ca1cce43973827fe94570cb47
tomas unzue <tomas.unzue03@gmail.com>
2023-01-23 23:10:38 -0300

    practicando

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+
+//let NombreUsuario = prompt("Decime tu nombre");
+//let ApellidoUsuario = prompt("Decime tu apellido");
+
+
+//let datosUser = "Hola tu nombre es : " + NombreUsuario + " " + ApellidoUsuario; 
+//alert(datosUser);
+
+//let EdadUsuario = prompt("Decime tu edad");
+
+//let EdadUser = "Tu edad es : " + EdadUsuario ;
+
+//alert(EdadUser);
+
+
+
+
+
+
+let edad = prompt ("ingrese su edad");
+
+if (edad =+ 18){
+    alert("sos mayor de edad, podes ingresar");
+} else {
+    alert("sos menor de edad, no podes ingresar");
+}
+
+let NombreUser = prompt("ingrese su nombre");
+
+let ApellidoUser = prompt("ingrese su apellido");
+
+let DatosUser = " tu nombre es : " + NombreUser + " " + ApellidoUser;
+
+alert (DatosUser);
