0e243408f188adee7a8e153d39726cb42dedd79e
tomas unzue <tomas.unzue03@gmail.com>
2023-03-15 00:58:49 -0300

    correccion

--- js/main.js
+++ js/main.js
@@ -20,8 +20,8 @@ const nombre = document.getElementById('nombre').value;
 const cantidadPersonas = document.getElementById('cantidadPersonas').value;
 const horario = document.getElementById('horario').value
 const plato1 = document.getElementById('plato1').value;
-const plato2 = document.getElementById('plato1').value;  
-const plato3 = document.getElementById('plato1').value;    
+const plato2 = document.getElementById('plato2').value;  
+const plato3 = document.getElementById('plato3').value;    
 const local = document.getElementById('local').value
 const confirmar = document.getElementById('confirmar').value
 const guardarReserva = document.getElementById('guardarReserva').value
@@ -38,7 +38,16 @@ const reserva = {
     local : local,
     confirmar: confirmar,
     guardarReserva : guardarReserva,
+    horario : horario,
     };
+
 reservas.push(reserva);
-localStorage.setItem('reservas', JSON.stringify(reservas));
+localStorage.setItem('reservas', JSON.stringify(reserva));
 }
+
+const form = document.getElementById("cancelar")
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(cancelar)
+} 
+)
